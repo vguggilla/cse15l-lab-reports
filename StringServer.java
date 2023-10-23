3ba820b7b516d86297db242fd28778a4c5d9088a
vguggilla <146775586+vguggilla@users.noreply.github.com>
2023-10-22 23:05:52 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -4,7 +4,7 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    String output = 0;
+    String output = "";
     int num = 0;
     public String handleRequest(URI url) {
         if (url.getPath().equals("/add-message")) {
